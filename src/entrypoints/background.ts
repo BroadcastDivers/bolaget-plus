da96499cc945fde5bde9f03956ae8a903f179ee7
Lemorz56 <sebastian@msbrg.net>
2025-01-25 01:30:33 +0100

    feat: Finalize untappd integration
    Closes #4

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,12 +1,12 @@
-import browser from 'webextension-polyfill'
-import { type GetRating, ProductType } from '@/@types/types'
+import { type RatingRequest, ProductType } from '@/@types/types'
 import { fetchRatingFromVivino } from '@/components/api'
+import browser from 'webextension-polyfill'
 
 export default defineBackground(() => {
   console.log('Running with id:', { id: browser.runtime.id })
 })
 
-function isGetRatingMessage(message: unknown): message is GetRating {
+function isGetRatingMessage(message: unknown): message is RatingRequest {
   return (
     typeof message === 'object' &&
     message !== null &&
@@ -19,11 +19,11 @@ browser.runtime.onMessage.addListener(async (message: unknown) => {
   if (!isGetRatingMessage(message)) {
     return
   }
-  const { query, productName } = message
+  const { productName, query } = message
   switch (query) {
-    case ProductType.Wine:
-      return await fetchRatingFromVivino(productName)
     case ProductType.Beer:
       return await fetchRatingFromUntappd(productName)
+    case ProductType.Wine:
+      return await fetchRatingFromVivino(productName)
   }
 })
--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,45 +1,64 @@
-import sentinel from 'sentinel-js'
-import * as domUtils from '@/components/domUtils'
-import { fetchRatingFromUntappd, fetchRating } from '@/components/api'
-import { translations } from '../translations'
-import * as productUtils from '@/components/productUtils'
-import { wineFeatureEnabled } from '@/components/settings'
 import {
   ProductType,
-  RatingResultStatus,
-  type RatingResponse
+  type RatingResponse,
+  RatingResultStatus
 } from '@/@types/types'
+import { fetchRating, fetchRatingFromUntappd } from '@/components/api'
+import * as domUtils from '@/components/domUtils'
+import * as productUtils from '@/components/productUtils'
+import { wineFeatureEnabled } from '@/components/settings'
+import sentinel from 'sentinel-js'
+
+import { translations } from '../translations'
 
 // TODO: this is required for WXT, look into it or make it fetch settings from storage?
 export default defineContentScript({
-  matches: ['*://*.systembolaget.se/*'],
   main() {
     sentinel.on('h1', tryInsertOnProdcutPage)
-  }
+  },
+  matches: ['*://*.systembolaget.se/*']
 })
 
 let fetchingRatingInProgress = false
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-async function tryInsertOnProdcutPage(_: any) {
-  // check if extension is enabled
-  const enabled = await featuresEnabled.getValue()
-  if (!enabled) {
-    return
+async function featureEnabled(productType: ProductType): Promise<boolean> {
+  if (
+    (productType === ProductType.Wine &&
+      !(await wineFeatureEnabled.getValue())) ||
+    (productType === ProductType.Beer && !(await beerFeatureEnabled.getValue()))
+  ) {
+    return false
   }
+  return true
+}
+
+async function handleRating(productType: ProductType, rating: RatingResponse) {
+  switch (rating.status) {
+    case RatingResultStatus.Found:
+      domUtils.setRating(productType, rating.rating, rating.votes, rating.link)
+      return
+    case RatingResultStatus.Uncertain:
+      domUtils.setUncertain(productType, rating.link)
+      return
+    default:
+      domUtils.setMessage(translations.noMatch)
+      return
+  }
+}
+
+async function tryInsertOnProdcutPage(_: any) {
+  if (!(await featuresEnabled.getValue())) return
 
   const productType = productUtils.getProductType()
-  if (fetchingRatingInProgress || productType === ProductType.Uncertain) {
+  if (
+    fetchingRatingInProgress ||
+    productType === ProductType.Uncertain ||
+    !(await featureEnabled(productType))
+  ) {
     return
   }
 
-  // FETCH SETTINGS - TODO:
-  const wineEnabled = wineFeatureEnabled.getValue()
-  const beerEnabled = beerFeatureEnabled.getValue()
-
   domUtils.injectRatingContainer()
-
   if (productType == ProductType.Wine && !productUtils.isBottle()) {
     domUtils.setMessage(translations.notOnBottle)
     return
@@ -61,19 +80,5 @@ async function tryInsertOnProdcutPage(_: any) {
     domUtils.setMessage(translations.noMatch)
   } finally {
     fetchingRatingInProgress = false
-  }
-}
-
-async function handleRating(productType: ProductType, rating: RatingResponse) {
-  switch (rating.status) {
-    case RatingResultStatus.Found:
-      domUtils.setRating(productType, rating.rating, rating.votes, rating.link)
-      return
-    case RatingResultStatus.Uncertain:
-      domUtils.setUncertain(productType, rating.link)
-      return
-    default:
-      domUtils.setMessage(translations.noMatch)
-      return
   }
 }
