0d7f5a0b8cda2d919fa8c70da680fd94e998309d
Lemorz56 <sebastian@msbrg.net>
2025-03-11 13:39:46 +0100

    feat: add i18n translation

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,50 +1,51 @@
-import { defineConfig } from 'wxt';
-const isProduction = process.env.NODE_ENV === 'production';
+import { defineConfig } from 'wxt'
+const isProduction = process.env.NODE_ENV === 'production'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/auto-icons'],
+  modules: ['@wxt-dev/auto-icons', '@wxt-dev/i18n/module'],
   runner: {
     startUrls: [
       'about:debugging#/runtime/this-firefox',
       'https://www.systembolaget.se/produkt/vin/onbrina-796501/',
-      'chrome://extensions/',
-    ],
+      'chrome://extensions/'
+    ]
   },
   dev: {
     server: {
-      port: 3000,
-    },
+      port: 3000
+    }
   },
   extensionApi: 'webextension-polyfill',
   srcDir: 'src',
   manifest: {
+    default_locale: 'sv',
     icons: {
       16: 'icons/16.png',
       32: 'icons/32.png',
       48: 'icons/48.png',
-      128: 'icons/128.png',
+      128: 'icons/128.png'
     },
     permissions: [
       'storage',
       '*://*.vivino.com/*',
       '*://*.untappd.com/*',
       'clipboardWrite',
-       // Conditionally include permissions based on the build environment
-       ...(isProduction ? [] : ['ws://localhost:3000/']),
+      // Conditionally include permissions based on the build environment
+      ...(isProduction ? [] : ['ws://localhost:3000/'])
     ],
 
     host_permissions: [
       'https://www.systembolaget.se/*',
       'https://www.vivino.com/*',
-      'https://untappd.com/*',
+      'https://untappd.com/*'
     ],
     content_security_policy: {
-      extension_pages: `script-src 'self'; object-src 'self'; connect-src 'self' https://www.vivino.com https://untappd.com${isProduction ? '' : ' ws://localhost:3000/'};`,
+      extension_pages: `script-src 'self'; object-src 'self'; connect-src 'self' https://www.vivino.com https://untappd.com${isProduction ? '' : ' ws://localhost:3000/'};`
     },
     browser_specific_settings: {
       gecko: {
-        id: 'broadcastdivers@test.com',
-      },
-    },
-  },
-});
+        id: 'broadcastdivers@test.com'
+      }
+    }
+  }
+})
